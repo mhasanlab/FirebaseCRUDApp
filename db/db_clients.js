f25cfaffa4c0205aff8f5845cd5556f5a9a65191
A.K.M Mainul Hasan <12734023-mainulhasanbd@users.noreply.gitlab.com>
2023-03-11 07:43:43 +0000

    Update db_clients.js

--- db/db_clients.js
+++ db/db_clients.js
@@ -42,7 +42,7 @@ async function readClient() {
           <td>${client.entryDate}</td>
           <td>
           <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#modalCenter" onclick="editClient('${doc.id}', '${client.name}', '${client.email}', '${client.phone}', '${client.status}', '${client.entryDate}')"><i class="bx bx-edit-alt me-1"></i></button>
-          <button type="button" class="btn btn-danger btn-sm" onclick="deleteClient('${doc.id}')"><i class="bx bx-trash me-1"></i></button>
+          <button type="button" class="btn btn-danger btn-sm" onsubmit="deleteClient('${doc.id}')"><i class="bx bx-trash me-1"></i></button>
           </td>
         </tr>`;
         clientList.insertAdjacentHTML("beforeend", row);
